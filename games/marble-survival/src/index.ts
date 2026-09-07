53d44821e9958ee3215d30dee0bb61f20309aca0
Paul Light <nwosupaul3@gmail.com>
2026-09-07 20:13:11 +0100

    feat(marble): add immutable sanitized presentation snapshot

--- games/marble-survival/src/index.ts
+++ games/marble-survival/src/index.ts
@@ -8,4 +8,6 @@ export * from './physics/solver';
 export * from './rules/tournament';
 export * from './runtime/run';
 export * from './persistence/snapshot';
+export * from './presentation/types';
+export * from './presentation/snapshot';
 export * from './manifest';
